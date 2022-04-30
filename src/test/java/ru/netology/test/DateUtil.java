3ca27b09d732c3558c96143acd13ed11fb2de4c6
Kristina Uldina <uldinakris@gmail.com>
2022-04-30 10:36:41 +0200

    modified test

--- src/test/java/ru/netology/test/DateUtil.java
+++ src/test/java/ru/netology/test/DateUtil.java
@@ -1,6 +1,7 @@
 package ru.netology.test;
 
 import java.time.LocalDate;
+import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
 
 public class DateUtil {
@@ -8,5 +9,11 @@ public class DateUtil {
         LocalDate date = LocalDate.now().plusDays(plusDays);
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern(formatArg);
         return date.format(formatter);
+    }
+
+    static String getTime(Integer plusDays) {
+        LocalDate date = LocalDate.now().plusDays(plusDays);
+        ZoneId zoneId = ZoneId.systemDefault();
+        return Long.toString(date.atStartOfDay(zoneId).toInstant().toEpochMilli());
     }
 }
--- src/test/java/ru/netology/test/TestWithFaker.java
+++ src/test/java/ru/netology/test/TestWithFaker.java
@@ -2,12 +2,15 @@ package ru.netology.test;
 
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.ElementsCollection;
+import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.time.Duration;
 import static com.codeborne.selenide.Selenide.*;
 import static ru.netology.test.DateUtil.getDate;
+import static ru.netology.test.DateUtil.getTime;
 
 public class TestWithFaker {
 
@@ -23,6 +26,7 @@ public class TestWithFaker {
     @Test
     void shouldSendForm() {
         String date = getDate(3, "dd.MM.yyyy");
+        String time = getTime(40);
 
         Configuration.holdBrowserOpen = true;
         open("http://localhost:9999");
@@ -35,12 +39,19 @@ public class TestWithFaker {
                 .should(Condition.visible, Duration.ofSeconds(15));
 
 
-
         $x("//button[contains(., 'Запланировать')]").click();
         $x("//*[contains(text(),'Необходимо подтверждение')]")
                 .should(Condition.visible, Duration.ofSeconds(15));
 
+        $x("//span[@data-test-id='date']//input").click();
 
+        ElementsCollection nextDateCellCollection = $$x("//td[@data-day='"+time+"']");
+        if (nextDateCellCollection.size() != 0) {
+            nextDateCellCollection.get(0).click();
+        } else {
+            $x("//div[contains(@class, 'calendar__arrow')][@data-step='1']").click();
+            $x("//td[@data-day='"+time+"']").click();
+        }
 
         $x("//button[contains(., 'Перепланировать')]").click();
         $x("//*[contains(text(),'Встреча успешно запланирована на')][contains(.,'" + date + "')]")
